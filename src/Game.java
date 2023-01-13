6b31df73b4437c33afd16b316b1dc3218ad285df
rhysdh540 <rhys_dehaan@ryecountryday.org>
2023-01-13 17:20:46 -0500

    no imports, less public static final String

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Game {
+public class Game {
+    // 0: empty, 1: X, 2: O
+    private int[][] board = new int[3][3];
+    // 0: X: 1: O
+    private int currentPlayer = 0;
+    public static final String RESET = "\033[0m", UNDERLINE = "\033[4m";
+
+    public Game() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+    public boolean play(int x, int y) {
+        if (board[x][y] == 0) {
+            board[x][y] = currentPlayer+1;
+            currentPlayer = (currentPlayer + 1) % 2;
+            return true;
+        }
+        return false;
+    }
+    public int isWon() {
+        if(board[0][0]==board[0][1] && board[0][1]==board[0][2] && board[0][0]!=0){
+            return board[0][0];
+        }
+        if(board[1][0]==board[1][1] && board[1][1]==board[1][2] && board[1][0]!=0){
+            return board[1][0];
+        }
+        if(board[2][0]==board[2][1] && board[2][1]==board[2][2] && board[2][0]!=0){
+            return board[2][0];
+        }
+        if(board[0][0]==board[1][0] && board[1][0]==board[2][0] && board[0][0]!=0){
+            return board[0][0];
+        }
+        if(board[0][1]==board[1][1] && board[1][1]==board[2][1] && board[0][1]!=0){
+            return board[0][1];
+        }
+        if(board[0][2]==board[1][2] && board[1][2]==board[2][2] && board[0][2]!=0){
+            return board[0][2];
+        }
+        if(board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[0][0]!=0){
+            return board[0][0];
+        }
+        if(board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[0][2]!=0){
+            return board[0][2];
+        }
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(board[i][j]==0)
+                    return 0;
+            }
+        }
+        return 3;
+    }
+    public int getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public void print(){
+        char[][] board = new char[3][3];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(this.board[i][j]==0){
+                    board[i][j] = ' ';
+                } else if(this.board[i][j]==1){
+                    board[i][j] = 'X';
+                } else {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+        System.out.println("  1   2   3");
+        for(int i = 0; i < 3; i++){
+            System.out.println(RESET + (i+1) + " " + (i==2 ? "" : UNDERLINE) + board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
+        }
+        System.out.print(RESET);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static final String RED = "\033[31m",
+            EMPTY_SCREEN = "\033[2J\033[H",
+            RESET = Game.RESET,
+            BOLD = "\033[1m",
+            GREEN = "\033[92m",
+            ORANGE = "\033[38;2;245;180;40m";
+
+    public static void main(String[]a){run();}
+    public static void run() {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        java.io.PrintStream so = System.out;
+        Game g = new Game();
+        so.println(EMPTY_SCREEN + "Welcome to Tic Tac Toe!");
+        so.println("Player 1 is X, Player 2 is O");
+        while(g.isWon()==0){
+            String input = "";
+            while(input.equals("") && g.isWon() == 0){
+                so.println("Player " + GREEN + BOLD + (g.getCurrentPlayer()+1) + RESET + "'s turn");
+                g.print();
+                so.print("Enter the coordinates of the square you want to play in (ex: " + RED + BOLD + "\"1, 1\"" + RESET + "): " + RED + BOLD);
+                input = sc.nextLine();
+                so.print(RESET);
+                try{
+                    String[] coords;
+                    String[] in1 = input.split(", ");
+                    String[] in2 = input.split(" ");
+                    String[] in3 = input.split(",");
+                    if(in1.length==2)
+                        coords = in1;
+                    else if(in2.length==2)
+                        coords = in2;
+                    else if(in3.length==2)
+                        coords = in3;
+                    else throw new Exception();
+                    int x = Integer.parseInt(coords[0]);
+                    int y = Integer.parseInt(coords[1]);
+                    if(!g.play(y-1,x-1)){
+                        so.print(EMPTY_SCREEN);
+                        so.println(RED + "That square is already taken!" + RESET);
+                        input = "";
+                    } else so.println(EMPTY_SCREEN);
+                } catch (Exception e){
+                    so.print(EMPTY_SCREEN);
+                    so.println(RED + "Invalid input!" + RESET);
+                    input = "";
+                }
+            }
+        }
+        if(g.isWon()==3){
+            so.println(ORANGE + "It's a draw!" + RESET);
+        } else {
+            so.println("Player " + GREEN + BOLD + g.isWon() + RESET + " won!");
+        }
+        g.print();
+        so.print("Would you like to play again? ");
+        String end = sc.nextLine();
+        if("y".equalsIgnoreCase(end) || "yes".equalsIgnoreCase(end))
+            new Thread(Main::run).start(); // avoid StackOverflowError in case somebody wants to play 10000 games of ttt
+        else sc.close();
     }
 }
